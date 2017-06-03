69516a9294579e123333efe415b7392f856faaee
geprinc <geprinc95@gmail.com>
2017-06-03 17:18:22 -0300

    Update Rocket.java
    algunos cambios

--- src/Rocket.java
+++ src/Rocket.java
@@ -3,7 +3,22 @@
  */
 public class Rocket extends GameObject{
 
-    public Rocket(int collisionWidth, int collisionHeight){
-        super(collisionWidth, collisionHeight);
+    private GameObject target;
+    private GameObject origin;
+    private static final float ROCKET_SPEED_FACTOR= 3;
+
+    public Rocket(int collisionWidth, int collisionHeight, GameObject target, GameObject origin){
+        super(collisionWidth, collisionHeight, ROCKET_SPEED_FACTOR);
+        this.target=target;
+        this.origin= origin;
     }
+    
+    public void setTarget (GameObject target){
+        this.target=target;
+    }
+    
+    public void updateTrayectory (){
+        
+    }
+    
 }
