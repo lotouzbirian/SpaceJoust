87b5ab7ea7bc59cdb36143b7a06667af4685b769
geprinc <geprinc95@gmail.com>
2017-06-03 17:16:43 -0300

    Update Ship.java
    agregue constantes y modifique cosntructor

--- src/Ship.java
+++ src/Ship.java
@@ -2,14 +2,16 @@
  * Created by Bensas on 5/27/17.
  */
 public class Ship extends GameObject{
-    private float radialPosition;
-
+    private float radialPosition;    
+    private static final float SHIP_SPEED_FACTOR= 2;
+    private static final float RADIUS=0;
     private boolean isAlive = true;
 
     public Ship(int collisionWidth, int collisionHeight){
-        super(collisionWidth, collisionHeight);
+        super(collisionWidth, collisionHeight, SHIP_SPEED_FACTOR);
 
     }
+    
     public float getRadialPosition(){return radialPosition;}
 
     public boolean isBehindShip(Ship otherShip){
