3c3cf652de1dd4ae3e2c2a6dd0ba883417089d48
josé Martín Torreguitar <jtorreguitar@itba.edu.ar>
2017-06-03 19:05:37 -0300

    agregue montones de constantes, getters, setters, acelerar y escudo

--- src/Ship.java
+++ src/Ship.java
@@ -2,19 +2,28 @@
  * Created by Bensas on 5/27/17.
  */
 public class Ship extends GameObject{
-    private float radialPosition;    
-    private static final float SHIP_SPEED_FACTOR= 2;
     private static final float RADIUS= 0;
+    private static final int STARTING_HEALTH= 5;
+    private static final int STARTING_ENERGY= 5;
+    private static final int ROCKET_COST= 2;
+    private static final int SHIELD_COST= 2;
+    private static final int ACCELERATION_COST= 2;
+    private static final int ACCELERATION_FACTOR=2;
+    private int shipSpeedFactor= 2;
+    private float radialPosition;    
     private boolean isAlive= true;
+    private int health= STARTING_HEALTH;
+    private int energy= STARTING_ENERGY;
+    private boolean shielded=false
 
     public Ship(int collisionWidth, int collisionHeight){
-        super(collisionWidth, collisionHeight, SHIP_SPEED_FACTOR);
+        super(collisionWidth, collisionHeight, shipSpeedFactor());
 
     }
 
     @Override
     public void update(){
-        setRadialPosition(getRadialPosition() + SHIP_SPEED_FACTOR);
+        setRadialPosition(getRadialPosition() + getshipSpeedFactor());
         setPositionX((int)(RADIUS * Math.cos(getRadialPosition())));
         setPositionY((int)(RADIUS * Math.sin(getRadialPosition())));
         super.update();
@@ -23,6 +32,35 @@ public class Ship extends GameObject{
     public float getRadialPosition(){return radialPosition;}
     public void setRadialPosition(float radialPosition){
         this.radialPosition= radialPosition;
+    }
+
+    public int getHealth(){return health;}
+    public void setHealth(int health){
+        this.health= health;
+    }
+
+    public int getEnergy(){return energy;}
+    public void setEnergy(){
+        this.energy= energy;
+    }
+
+    public int getshipSpeedFactor(){return shipSpeedFactor;}
+    public void setShipSpeedFactor(int speed){
+        shipSpeedFactor=speed;
+    }
+
+    public void accelerate(){
+        setShipSpeedFactor(getshipSpeedFactor() * ACCELERATION_FACTOR);
+    }
+    public void decelerate(){
+        setShipSpeedFactor(getshipSpeedFactor() / ACCELERATION_FACTOR);
+    }
+
+    public void shield(){
+        shielded=true;
+    }
+    public void unshield(){
+        shielded=false;
     }
 
     public boolean isBehindShip(Ship otherShip){
