939a2731db3743ae0fe5c2b2bbc7197e89498221
Bensas <juanbensa@gmail.com>
2017-06-19 22:12:14 -0300

    Pre entrega

--- src/AsteroidView.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Juan Bensadon
- * View del Asteroide.
- */
-public class AsteroidView extends GameObjectView{
-}
--- src/RocketView.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-import java.util.Observable;
-
-/**
- * @author Juan Bensadon
- */
-public class RocketView extends GameObjectView {
-}
